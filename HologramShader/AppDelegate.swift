400c81e51d4b0e3cfbf25546a8c129e708e42564
Nathan Petrangelo <nathan@petrangelo.org>
2022-03-21 15:25:59 -0400

    Generated Xcode Project

---
+++ HologramShader/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  HologramShader
+//
+//  Created by qe on 3/21/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
